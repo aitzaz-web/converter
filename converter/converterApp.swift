96aef84a34e5075574958e7b4ab5e8d1f397badf
Aitzaz Munir Shaikh <ams845@cornell.edugit config --global user.email ams845@cornell.edu>
2024-06-23 17:23:56 +0500

    Initial Commit

---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by Aitzaz Munir on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
